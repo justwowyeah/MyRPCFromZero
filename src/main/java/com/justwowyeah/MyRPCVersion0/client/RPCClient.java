68a161945d47208711b47258bece75f8183fc53b
justwowyeah <justwowyeah@gmail.com>
2024-05-30 20:32:27 +0800

    server finished

--- src/main/java/com/justwowyeah/MyRPCVersion0/client/RPCClient.java
+++ src/main/java/com/justwowyeah/MyRPCVersion0/client/RPCClient.java
@@ -10,23 +10,22 @@ import java.util.Random;
 
 public class RPCClient {
     public static void main(String[] args) {
-        try {
-            // 建立Socket连接
-            Socket socket = new Socket("127.0.0.1", 8899);
-            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-
+        // 建立Socket连接，创建流
+        try (Socket socket = new Socket("127.0.0.1", 8899);
+             ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream())) {
             Random random = new Random();
-            // 写入 int 类型到缓冲区
+            // 写入随机的 int 数据到缓冲区
             objectOutputStream.writeInt(random.nextInt());
-            // 清空缓冲区
+            // 不等缓冲区满，手动清空以发送数据
             objectOutputStream.flush();
             // 将接收到的网络字节流强转为 User 类型
             User user = (User) objectInputStream.readObject();
             System.out.println("从服务端接收到的User：" + user);
 
         } catch (IOException | ClassNotFoundException e) {
-            e.getStackTrace();
+            // 如果有 IO 异常，则打印堆栈追踪信息
+            e.printStackTrace();
             System.out.println("客户端启动失败");
         }
 
---
+++ src/main/java/com/justwowyeah/MyRPCVersion0/server/RPCServer.java
@@ -0,0 +1,46 @@
+package com.justwowyeah.MyRPCVersion0.server;
+
+import com.justwowyeah.MyRPCVersion0.service.UserService;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class RPCServer {
+    public static void main(String[] args) {
+        UserService userservice = new UserServiceImpl();
+
+        // 使用 try-with-resources 建立 ServerSocket 连接
+        try (ServerSocket serverSocket = new ServerSocket(8899)) {
+            System.out.println("服务端已启动");
+            while (true) {
+                // ServerSocket 以 BIO 监听 8899 端口，一旦获取到连接就创建 Socket 对象
+                Socket socket = serverSocket.accept();
+                // 异步创建 runnable 线程，使用 lamda 表达式重写 run()
+                new Thread(() -> {
+                    // 创建流
+                    try (ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream())) {
+                        // 从流中读取 int 数据，转换为 Integer 对象
+                        // 如果不转换，JVM 也会在 (userservice 为 id 赋值时) 隐式转换
+                        Integer id = ois.readInt();
+                        oos.writeObject(userservice.getUserById(id));
+                        // 不等缓冲区满，手动清空以发送数据
+                        oos.flush();
+                    } catch (IOException e) {
+                        // 如果有 IO 异常，则打印 run() 堆栈追踪信息
+                        e.printStackTrace();
+                        System.out.println("IO错误");
+                    }
+                // 使用 start() 调用 run() 运行新线程
+                }).start();
+            }
+        } catch (IOException e) {
+            // 如果有 IO 异常，则打印 serverSocket 堆栈追踪信息
+            e.printStackTrace();
+            System.out.println("服务端启动失败");
+        }
+    }
+}
